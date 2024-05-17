4ee2ec31b5b108e12b1dd476471096474e06bfba
Thiago Pereira <pereira.tp.thiago@gmail.com>
2024-05-17 15:44:23 -0300

    02-componentes e paginas - aula 4 componente subscribe button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Head from "next/head";
 
 import styles from './home.module.scss'
+import { SubscribeButton } from "../components/SubscribeButton";
 
 export default function Home() {
   return (
@@ -19,6 +20,7 @@ export default function Home() {
             Get access to all the publications <br />
             <span>for $9.90 month</span>
           </p>
+          <SubscribeButton />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
