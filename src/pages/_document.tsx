a4061802c2934d65a2acba6bc85f9f9d8dbc30c9
Thiago Pereira <pereira.tp.thiago@gmail.com>
2024-05-17 14:21:06 -0300

    01-estrutura da aplicacao - aula 9 - title dinamico por pagina

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,9 +5,7 @@ export default function Document() {
     <Html lang="pt-BR">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
-        <title>ig.news</title>
-        
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />        
       </Head>
       <body>
         <Main />
