74dbe1baaed29a1775fa0fdc76c83005030d5db9
Thiago Pereira <pereira.tp.thiago@gmail.com>
2024-05-20 16:13:04 -0300

    02-backend no frontend - aula 3 - salvando usuario no banco de dados

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { fauna } from "../../../services/fauna"
+import { query as q } from "faunadb"
 
 export const authOptions = {
   providers: [
@@ -13,6 +15,23 @@ export const authOptions = {
       },
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile, credentials }) {
+      const { email } = user
+      
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email }}
+          )
+        )
+        return true
+      } catch (err) {
+        return false
+      }
+    },
+  }
 }
 
 export default NextAuth(authOptions)
