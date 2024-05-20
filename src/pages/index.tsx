7d1ba0148a524a081a75d8a845882d7aa74de2e7
Thiago Pereira <pereira.tp.thiago@gmail.com>
2024-05-20 13:30:57 -0300

    03-integracao com a API - aula 3-static site generation(SSG)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { GetServerSideProps } from "next";
+import { GetServerSideProps, GetStaticProps } from "next";
 import Head from "next/head";
 
 import styles from './home.module.scss'
@@ -38,7 +38,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1PHVu8DFXV3PGo0GIytpXc3j', {
     expand: ['product']
   })
@@ -54,6 +54,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: { 
       product
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
   }
 }
