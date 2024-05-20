1af76c355cb91e3d570574cabd5900072e69fe5b
Thiago Pereira <pereira.tp.thiago@gmail.com>
2024-05-20 15:24:39 -0300

    04-API routes no nextjs - aula 4 - autenticacao com next auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,13 @@ import React from "react";
 
 import '../styles/global.scss'
 import { Header } from "../components/Header";
+import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <>
+    <SessionProvider session={session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   );
 }
