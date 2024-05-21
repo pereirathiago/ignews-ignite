0a134f54c04dfb8bbbf9434a3e0a7bfbf0c3e6e2
Thiago Pereira <pereira.tp.thiago@gmail.com>
2024-05-21 13:01:45 -0300

    04-ouvindo webhooks - aula 4 - ouvindo mais eventos

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -5,15 +5,15 @@ import { stripe } from "../../../services/stripe";
 export async function saveSubscription(
   subscriptionId: string,
   customerId: string,
+  createAction = false
 ) {
-  // console.log(subscriptionId, customerId)
   const userRef = await fauna.query(
     q.Select(
-      'ref',
+      "ref",
       q.Get(
         q.Match(
           q.Index('user_by_stripe_customer_id'),
-          customerId,
+          customerId
         )
       )
     )
@@ -25,13 +25,30 @@ export async function saveSubscription(
     id: subscription.id,
     userId: userRef,
     status: subscription.status,
-    price_id: subscription.items.data[0].price.id
+    price_id: subscription.items.data[0].price.id,
   }
 
-  await fauna.query(
-    q.Create(
-      q.Collection('subscriptions'),
-      { data: subscriptionData }
+  if(createAction) {
+    await fauna.query(
+      q.Create(
+        q.Collection('subscriptions'),
+        { data: subscriptionData }
+      )
     )
-  )
+  } else {
+    await fauna.query(
+      q.Replace(
+        q.Select(
+          "ref", 
+          q.Get(
+            q.Match(
+              q.Index('subscription_by_id'),
+              subscriptionId
+            )
+          )
+        ),
+        { data: subscriptionData }
+      ),
+    )
+  }
 }
