1b39d043cbc595cf408f2523bca7adcefc6931c3
Thiago Pereira <pereira.tp.thiago@gmail.com>
2024-05-21 15:04:29 -0300

    01-trabalhando com CMS - aula 5- listando posts em tela

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -2,8 +2,20 @@ import Head from 'next/head';
 import styles from './styles.module.scss';
 import { GetStaticProps } from 'next';
 import { prismic } from '../../services/prismic';
+import { RichText } from 'prismic-dom'
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+
+interface PostsProps {
+  posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -12,27 +24,13 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="">
-            <time>21 de maio de 2024</time>
-            <strong>Creating a monorepo with lerda</strong>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit in inventore facilis unde cupiditate quis minima illum nesciunt corporis iusto beatae, aperiam, omnis temporibus doloribus mollitia officia</p>
-          </a>
-          <a href="">
-            <time>21 de maio de 2024</time>
-            <strong>Creating a monorepo with lerda</strong>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit in inventore facilis unde cupiditate quis minima illum nesciunt corporis iusto beatae, aperiam, omnis temporibus doloribus mollitia officia</p>
-          </a>
-          <a href="">
-            <time>21 de maio de 2024</time>
-            <strong>Creating a monorepo with lerda</strong>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit in inventore facilis unde cupiditate quis minima illum nesciunt corporis iusto beatae, aperiam, omnis temporibus doloribus mollitia officia</p>
-          </a>
-          <a href="">
-            <time>21 de maio de 2024</time>
-            <strong>Creating a monorepo with lerda</strong>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit in inventore facilis unde cupiditate quis minima illum nesciunt corporis iusto beatae, aperiam, omnis temporibus doloribus mollitia officia</p>
-          </a>
-          
+          {posts.map(post => (
+            <a href="" key={post.slug}>
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -45,11 +43,24 @@ export const getStaticProps: GetStaticProps = async () => {
     pageSize: 100,
   })
 
-  console.log(response)
+  // console.log(JSON.stringify(response, null, 2))
+
+  const posts = response.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    }
+  })
 
   return {
     props: {
-      response
+      posts
     }
   }
 }
