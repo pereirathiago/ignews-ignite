f3a1e10d46d735f869470853b80c028773b63e6f
Thiago Pereira <pereira.tp.thiago@gmail.com>
2024-05-21 16:51:33 -0300

    02-paginas estaticas - aula 3 - pagina preview do post

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -36,10 +36,8 @@ export default function Post({ post }: PostProps) {
 export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
   const session = await getSession({ req }) as any
   const { slug } = params
-  
-  // console.log(session)
 
-  if(!session.activeSubscription) {
+  if(!session?.activeSubscription) {
     return {
       redirect: {
         destination: '/',
