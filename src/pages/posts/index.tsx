de002f1c3448be9653c1e199fa755244def56f1d
Thiago Pereira <pereira.tp.thiago@gmail.com>
2024-05-21 15:51:24 -0300

    02-paginas estaticas - aula 1 - pagina: post

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -3,6 +3,7 @@ import styles from './styles.module.scss';
 import { GetStaticProps } from 'next';
 import { prismic } from '../../services/prismic';
 import { RichText } from 'prismic-dom'
+import Link from 'next/link';
 
 type Post = {
   slug: string;
@@ -25,11 +26,11 @@ export default function Posts({ posts }: PostsProps) {
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts.map(post => (
-            <a href="" key={post.slug}>
+            <Link href={`/posts/${post.slug}`} key={post.slug}>
               <time>{post.updatedAt}</time>
               <strong>{post.title}</strong>
               <p>{post.excerpt}</p>
-            </a>
+            </Link>
           ))}
         </div>
       </main>
